3950ea5c7708c64d4884c5b473b576917cbea298
Sambhav <samdkk9852@gmail.com>
2023-09-08 12:04:59 +0530

    create Subtotal Component and added react-currency-format to it(It is still static)

--- src/App.js
+++ src/App.js
@@ -2,17 +2,24 @@ import './App.css';
 import React from 'react';
 import Header from './Header';
 import Home from './Home';
+import { BrowserRouter, Routes ,Route} from 'react-router-dom';
+import Checkout from './Checkout';
 
 function App() {
   return (
      // BEM
-    <div className="app">   
-      {/* Header */}
-      <Header/>
-      
-      {/* Home */}
-      <Home/>
-    </div>
+    <BrowserRouter>
+      <div className="app">
+        <Header />
+        <Routes>
+          <Route path='/checkout' element={<Checkout />}/>
+        </Routes>
+        <Routes>
+          <Route path='/' element={<Home />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+   
   );
 }
 
--- src/Header.jsx
+++ src/Header.jsx
@@ -3,12 +3,15 @@ import Amazon_Logo from "./image/Amazon_Logo.png"
 import "./Header.css"
 import SearchIcon from '@mui/icons-material/Search';
 import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <div className="header">
-      <img className="header__logo" src={Amazon_Logo} alt="amazon_logo" />
-
+        <Link to="/">
+          <img className="header__logo" src={Amazon_Logo} alt="amazon_logo" />
+        </Link>
+      
       <div className="header__search">
         <input className="header__searchInput" type="text" />
         {/* Search_icon */}
@@ -28,11 +31,12 @@ const Header = () => {
           <span className="header__optionLineOne">Your</span>
           <span className="header__optionLineTwo">Prime</span>
         </div>
-        <div className="header__optionBasket">
-          <ShoppingBasketIcon />
-          <span className="header__optionLineTwo header__basketCount">0</span>
-
-        </div>
+        <Link to="/checkout">
+          <div className="header__optionBasket">
+            <ShoppingBasketIcon />
+            <span className="header__optionLineTwo header__basketCount">0</span>
+          </div>
+        </Link>
 
       </div>
 
